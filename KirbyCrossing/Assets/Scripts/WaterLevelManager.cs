179bb740244d71506e686b0e524c86a8acd3920b
alexwang05 <hpwzz20@gmail.com>
2021-07-09 22:43:12 -0700

    add rising and lowering water routine

--- KirbyCrossing/Assets/Scripts/WaterLevelManager.cs
+++ KirbyCrossing/Assets/Scripts/WaterLevelManager.cs
@@ -6,11 +6,13 @@ public class WaterLevelManager : MonoBehaviour
 {
     public GameObject waterPlanePrefab;
     public float playedTime;
-    private float waterHeight;
-    private float maxWaterHeight = 10;
+    public float waterHeight;
+    private float startWaterHeight = -0.5f;
+    // private float maxWaterHeight = -0.01f;
+    private float maxWaterHeight = 10f;
     private GameObject waterPlane;
 
-
+    private bool risingWater = true;
 
     // Start is called before the first frame update
     void Start()
@@ -18,32 +20,54 @@ public class WaterLevelManager : MonoBehaviour
         CreateWaterLevel();
         waterPlane = GameObject.FindGameObjectWithTag("Water Plane");
         waterHeight = waterPlane.transform.position.y;
-
-        StartCoroutine(RaiseWaterLevelRoutine());
+        
+        InvokeRepeating("ChangeWaterLevel", 0, 0.01f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        // set water plane height
         waterHeight = waterPlane.transform.position.y;
     }
 
-    IEnumerator RaiseWaterLevelRoutine()
+    void ChangeWaterLevel()
     {
-        // wait for 5 second
-        yield return new WaitForSeconds(5);
-
-        while (waterHeight < maxWaterHeight)
+        // if water is at max height or is going down
+        if (waterHeight == maxWaterHeight || !risingWater)
         {
-            waterPlane.transform.Translate(Vector3.up);
+            DecreaseWaterLevel();
         }
-        
+        else if (waterHeight == startWaterHeight || risingWater)
+        {
+            IncreaseWaterLevel();
+        }
+    }
+
+    void IncreaseWaterLevel()
+    {
+        Vector3 endLocation = new Vector3(waterPlane.transform.position.x, maxWaterHeight, waterPlane.transform.position.z);
+
+        // move water plane up
+        waterPlane.transform.position = Vector3.MoveTowards(waterPlane.transform.position, endLocation, Time.deltaTime / 10);
+
+        // should the water be rising
+        risingWater = !(waterHeight == maxWaterHeight);   
+    }
+
+    void DecreaseWaterLevel()
+    {
+        Vector3 endLocation = new Vector3(waterPlane.transform.position.x, startWaterHeight, waterPlane.transform.position.z);
+
+        // move water plane up
+        waterPlane.transform.position = Vector3.MoveTowards(waterPlane.transform.position, endLocation, Time.deltaTime / 10);
+
+        // should the water be rising
+        risingWater = (waterHeight == startWaterHeight);
     }
 
     void CreateWaterLevel()
     {
-        Instantiate(waterPlanePrefab, new Vector3(0, -0.5f, 0), Quaternion.identity);
+        Instantiate(waterPlanePrefab, new Vector3(0, startWaterHeight, 0), Quaternion.identity);
     }
 
 
