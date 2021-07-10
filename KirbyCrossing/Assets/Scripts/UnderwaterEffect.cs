4c0377438b9379476adde409e5e03d33c640cc84
alexwang05 <hpwzz20@gmail.com>
2021-07-09 19:40:18 -0700

    add Water Level Manager and Underwater Effects

---
+++ KirbyCrossing/Assets/Scripts/UnderwaterEffect.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnderwaterEffect : MonoBehaviour
+{
+    public float waterEffectHeight = -0.7f;
+    private bool isUnderwater;
+    private Color normalColor;
+    private Color underwaterColor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        normalColor = new Color(0.5f, 0.5f, 0.5f, 0.5f);
+        underwaterColor = new Color(0f, 0.4f, 0.7f, 0.6f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // when player gets in/out of water
+        if ((transform.position.y < waterEffectHeight) != isUnderwater)
+        {
+            isUnderwater = transform.position.y < waterEffectHeight;
+            
+            if (isUnderwater)
+                SetUnderwater();
+            if (!isUnderwater) 
+                SetNormal();
+        }
+    }
+
+
+    void SetNormal()
+    {
+        RenderSettings.fog = false;
+
+        RenderSettings.fogColor = normalColor;
+        RenderSettings.fogDensity = 0.01f;
+    }
+
+    void SetUnderwater()
+    {
+        RenderSettings.fog = true;
+        RenderSettings.fogColor = underwaterColor;
+        RenderSettings.fogDensity = 0.1f;
+
+    }
+
+    
+}
---
+++ KirbyCrossing/Assets/Scripts/WaterLevelManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterLevelManager : MonoBehaviour
+{
+    public GameObject waterPlanePrefab;
+    public float playedTime;
+    private float waterHeight;
+    private float maxWaterHeight = 10;
+    private GameObject waterPlane;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreateWaterLevel();
+        waterPlane = GameObject.FindGameObjectWithTag("Water Plane");
+        waterHeight = waterPlane.transform.position.y;
+
+        StartCoroutine(RaiseWaterLevelRoutine());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // set water plane height
+        waterHeight = waterPlane.transform.position.y;
+    }
+
+    IEnumerator RaiseWaterLevelRoutine()
+    {
+        // wait for 5 second
+        yield return new WaitForSeconds(5);
+
+        while (waterHeight < maxWaterHeight)
+        {
+            waterPlane.transform.Translate(Vector3.up);
+        }
+        
+    }
+
+    void CreateWaterLevel()
+    {
+        Instantiate(waterPlanePrefab, new Vector3(0, -0.5f, 0), Quaternion.identity);
+    }
+
+
+}
