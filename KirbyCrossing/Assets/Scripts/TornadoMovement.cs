cfabe72ab9462be7e567a30167a4c27770c3f5c8
alexwang05 <hpwzz20@gmail.com>
2021-07-10 22:16:37 -0700

    add tornado

---
+++ KirbyCrossing/Assets/Scripts/TornadoMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TornadoMovement : MonoBehaviour
+{
+    public GameObject tornadoPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreateTornado();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void CreateTornado()
+    {
+        Vector3 spawnLocation1 = new Vector3(-40, 5, -60);
+        Vector3 spawnLocation2 = new Vector3(40, 5, 60);
+
+        // instantiate 2 tornados
+        Instantiate(tornadoPrefab, spawnLocation1, Quaternion.identity);
+        Instantiate(tornadoPrefab, spawnLocation2, Quaternion.identity);
+    }
+}
