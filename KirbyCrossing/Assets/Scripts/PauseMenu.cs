0d8a8e13645c742dfe7503e9f356bb3ca05210ad
alexwang05 <hpwzz20@gmail.com>
2021-07-15 16:26:32 -0700

    add non-working pause menu

---
+++ KirbyCrossing/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+// using Microsoft.MixedReality.Toolkit;
+// using Microsoft.MixedReality.Toolkit.SceneSystem;
+
+public class PauseMenu : MonoBehaviour
+{
+    // public IMixedRealitySceneSystem sceneSystem;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // sceneSystem = MixedRealityToolkit.Instance.GetService<IMixedRealitySceneSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            // UnloadCurrentScene();
+            SceneManager.LoadScene("Menu");
+        }
+    }
+
+    public async void UnloadCurrentScene()
+    {
+        // await sceneSystem.UnloadContent("Flood");
+    }
+}
